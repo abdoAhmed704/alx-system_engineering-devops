6c994c21f0ac77845ca74946b59570968fa3cf3d
= <abdoahmedsayed704@gmail.com>
2024-02-01 12:36:45 +0200

    Done..

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
-str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
-str.split(/\w+:(.*), to:(.*), flags:(.*)/)
-puts str
+
+def parse_log_entry(log_entry)
+  match_data = log_entry.match(/\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/)
+  if match_data
+    sender = match_data[:sender].strip
+    receiver = match_data[:receiver].strip
+    flags = match_data[:flags].strip
+    puts "#{sender},#{receiver},#{flags}"
+  else
+    puts "Invalid log entry format"
+  end
+end
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <log_entry>"
+else
+  log_entry = ARGV[0]
+  parse_log_entry(log_entry)
+end
+
