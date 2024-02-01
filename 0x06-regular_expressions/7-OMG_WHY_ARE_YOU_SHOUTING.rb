6bbe1362a1f96610e0873eaeb40efa7c17197a0a
= <abdoahmedsayed704@gmail.com>
2024-02-01 12:35:40 +0200

    Done..

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,21 +1,5 @@
 #!/usr/bin/env ruby
-
-def parse_log_entry(log_entry)
-  match_data = log_entry.match(/\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/)
-  if match_data
-    sender = match_data[:sender].strip
-    receiver = match_data[:receiver].strip
-    flags = match_data[:flags].strip
-    puts "#{sender},#{receiver},#{flags}"
-  else
-    puts "Invalid log entry format"
-  end
-end
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <log_entry>"
-else
-  log_entry = ARGV[0]
-  parse_log_entry(log_entry)
-end
-
+#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
+str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
+str.split(/\w+:(.*), to:(.*), flags:(.*)/)
+puts str
