bee2ebd6e7f7895acdd42a9d12507094067c3258
= <abdoahmedsayed704@gmail.com>
2024-02-01 12:36:14 +0200

    Done..

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
 str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
 str.split(/\w+:(.*), to:(.*), flags:(.*)/)
 puts str
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
-str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
-str.split(/\w+:(.*), to:(.*), flags:(.*)/)
-puts str
