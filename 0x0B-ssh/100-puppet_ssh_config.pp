9de1a0f4274a7f2505fdff0ffb5159f91e9a8913
= <abdoahmedsayed704@gmail.com>
2024-03-24 23:24:56 +0200

    AJJ

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# cccccccccccccc
+file_line { 'Identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+file_line { 'disable password login':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+}
